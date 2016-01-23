d439797d57f588ec154a3d592470505d6d7f8b4b
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2016-01-23 12:28:05 +0300

    Better test.

--- examples/github-go/activity_test.go
+++ examples/github-go/activity_test.go
@@ -6,7 +6,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/antonholmquist/jason"
+	"github.com/stretchr/testify/require"
 
 	"github.com/gophergala2016/gophers"
 )
@@ -18,12 +19,36 @@ var (
 func TestListOrgs(t *testing.T) {
 	req := Client.NewRequest(t, "GET", "/user/orgs")
 	b, err := httputil.DumpRequestOut(req, true)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	t.Logf("Request:\n%s", b)
 
 	resp, err := http.DefaultClient.Do(req)
-	assert.Nil(t, err)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	require.Nil(t, err)
 	b, err = httputil.DumpResponse(resp, true)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	t.Logf("Response:\n%s", b)
+
+	v, err := jason.NewValueFromReader(resp.Body)
+	require.Nil(t, err)
+	a, err := v.Array()
+	require.Nil(t, err)
+
+	var found bool
+	for _, v = range a {
+		o, err := v.Object()
+		require.Nil(t, err)
+		t.Log(o)
+
+		login, err := o.GetString("login")
+		require.Nil(t, err)
+		if login == "gophergala2016" {
+			found = true
+			break
+		}
+	}
+
+	require.True(t, found)
 }
