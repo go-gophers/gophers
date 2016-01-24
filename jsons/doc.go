627df60b763adfb9fd07df0827a42d75c7af1d29
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2016-01-24 21:33:47 +0300

    Extract package documentation.

---
+++ jsons/doc.go
@@ -0,0 +1,8 @@
+// Package jsons allows sloppy work with JSON structures (objects and arrays).
+//
+// This package is tailored for usage in tests, specifically for Gophers tool.
+// It sacrifices idiomatic approach to brevity and simplicity of usage.
+// For example, all methods panic instead of returning error.
+//
+// Usage of this package in non-testing code is highly unadvisable.
+package jsons
