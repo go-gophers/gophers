b0f281e2a547e13fdee8c1b290fb3ab90f33ebf5
Mikhail Salosin <salosinm@gmail.com>
2016-05-17 17:05:13 +0400

    Add disable recorder flag (#17)

--- flags.go
+++ flags.go
@@ -7,8 +7,9 @@ import (
 )
 
 var (
-	noColorsF = flag.Bool("gophers.no-colors", false, "Disable requests and responses coloring")
-	vF        = flag.Bool("gophers.v", false, "Log to stderr full requests and responses")
+	noColorsF        = flag.Bool("gophers.no-colors", false, "Disable requests and responses coloring")
+	vF               = flag.Bool("gophers.v", false, "Log to stderr full requests and responses")
+	disableRecorderF = flag.Bool("gophers.disable-recorder", false, "Disable requests and responses recorder")
 )
 
 func init() {
