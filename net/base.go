9db810bb337191c53e3c548149c3343c63dbb4cc
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2016-09-27 15:24:50 +0300

    Change values of «error» label.

--- net/base.go
+++ net/base.go
@@ -1,8 +1,17 @@
 package net
 
+import (
+	"io"
+)
+
+// errorLabelValue returns value for Prometheus metric label "error":
+// "ok" for no error, "EOF" for io.EOF, "error" otherwise.
 func errorLabelValue(err error) string {
 	if err == nil {
-		return "0"
+		return "ok"
 	}
-	return "1"
+	if err == io.EOF {
+		return "EOF"
+	}
+	return "error"
 }
